e385972db532dae05362835e3ccebfe883363deb
GilsonFonseca <gilson@ice.ufjf.br>
2017-04-26 04:51:57 -0300

    Added files

---
+++ root/webpack.config.js
@@ -0,0 +1,28 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: "index_bundle.js"
+  },
+  module: {
+    loaders: [
+      {test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+		  }
+	  } 
+    ]
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
